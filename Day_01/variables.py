9323cdd6fae227d1739e2e5c751cc5e4c06c481a
Hari <56271057+Harikalatheeswaran@users.noreply.github.com>
2026-05-02 20:12:56 +0530

    Add files via upload

---
+++ Day_01/variables.py
@@ -0,0 +1,14 @@
+my_bio = '''
+hello, my name is Eashan.
+I like to play foot ball.
+I love to eat mutton biriyani
+my age is 14
+'''
+
+# printing the bio
+# print("chota bheem")
+
+print(my_bio)
+
+
+
