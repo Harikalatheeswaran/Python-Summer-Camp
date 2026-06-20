a9057620a73cddb1197714cdc19125b2b6cacc2d
Hari <56271057+Harikalatheeswaran@users.noreply.github.com>
2026-06-20 20:39:28 +0530

    Add files via upload

---
+++ Day_06/Understanding_if_condition.py
@@ -0,0 +1,21 @@
+
+b = input("enter the number : ") # taking the input from the user
+c = int(b) # convert into integer
+
+# c is not converting to integer
+# int() function is converting it to the integer, then we store it inside c
+
+if c > 56:
+    print(c, "is greater thatn 56") # the space after if is called as INDENDATION.
+    print("green")
+    print("Purlple")
+    print("blue")
+
+if c < 56:
+    print(c, "is less than 56")
+    print("chicken biriyani")
+    print("gobi")
+    print("chicken 65")
+
+print("Eashan & Charvi are learning python")
+print("they are very smart!")
